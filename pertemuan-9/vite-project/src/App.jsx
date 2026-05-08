518b73a6f0d6fa41fd48e4b902f741c0d76cd15d
Kirasy42 <hanifanaktasik@gmail.com>
2026-05-08 14:42:56 +0700

    pertemuan 9

---
+++ pertemuan-9/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <>
+    <div>
+      <h2>This is Create React Vite</h2>
+      <Hello name="Hanif"/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ pertemuan-9/vite-project/src/components/Hello.jsx
@@ -0,0 +1,12 @@
+function Hello(props) {
+    const {name} = props;
+    
+    return (
+        <div>
+            <h1>Hello, React!</h1>
+            <h2>Saya {name} - Frontend Engineer</h2>
+        </div>
+    )
+}
+
+export default Hello;
